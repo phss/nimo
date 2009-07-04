4040a9b9299499c83ea1625a9224d7b8c1174e9b
moonpxi <paulo.schneider@gmail.com>
2009-07-04 15:00:54 +0100

    Finishing soundy example

--- examples/soundy.rb
+++ examples/soundy.rb
@@ -13,12 +13,38 @@ WINDOW_HEIGHT = 480
 
 class MainScreen < Nimo::Screen
   
-  def on_enter
-    resource_loader.load_sound("examples/sounds/KDE-Sys-Log-Out.ogg").play
+  def load    
+    self.when(:on_enter) { @resources.sounds[:startup].play(true) }
+    
+    add(Nimo::TextRepresentation, :with => {:x => 10, :y => 10, :font => :main, :color => Gosu::white,
+      :text => "This screen will play a background sound."})
+    add(Nimo::TextRepresentation, :with => {:x => 10, :y => 28, :font => :main, :color => Gosu::white,
+      :text => "Press <esc> to quit or any other key to go to the next screen."})      
   end
   
   def button_down(id)
-    exit
+    if id == Gosu::Button::KbEscape
+      exit 
+    else
+      @resources.sounds[:startup].stop
+      go_to(:Another)
+    end
+  end
+  
+end
+
+class AnotherScreen < Nimo::Screen
+  
+  def load
+    add(Nimo::TextRepresentation, :with => {:x => 10, :y => 10, :font => :main, :color => Gosu::white,
+      :text => "This screen will play a sound effect."})
+    add(Nimo::TextRepresentation, :with => {:x => 10, :y => 28, :font => :main, :color => Gosu::white,
+      :text => "Press <esc> to go back or any other key to play the sound."})
+  end
+  
+  def button_down(id)
+    go_to(:Main) if id == Gosu::Button::KbEscape
+    @resources.sounds[:effect].play
   end
   
 end
@@ -26,6 +52,10 @@ end
 
 if __FILE__ == $PROGRAM_NAME
   window = Nimo::GameWindow.new("Soundy", WINDOW_WIDTH, WINDOW_HEIGHT)
-  window.add_screens_by_class(MainScreen)
+  window.global_resources.
+    with_sound(:startup, "examples/sounds/KDE-Sys-Log-Out.ogg").
+    with_sound(:effect, "examples/sounds/k3b_error1.wav").    
+    with_font(:main, "Helvetica", 15)
+  window.add_screens_by_class(MainScreen, AnotherScreen)
   window.show
 end

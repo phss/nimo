ef08fa49a489f2bc9b05c83d6562599ad56b436a
moonpxi <paulo.schneider@gmail.com>
2009-08-20 22:31:48 +0100

    Adding simplest example, with the new DSL

--- examples/bouncy.rb
+++ examples/bouncy.rb
@@ -43,24 +43,23 @@ WINDOW_HEIGHT = 480
 
 class GameScreen < Nimo::Screen
   
-  representations do
-    balls.each do |ball|
-      representation Nimo::Quad, :for => ball, :with => {:color => ball.color} do
-        always { move }
-        listen_to(:color_change) { |representation, object| representation.color = ball.color }
-      end
-    end
-
-    representation(Nimo::Quad, :for => pad, :with => {:color => Gosu::white}) do
-      when_key(Gosu::Button::KbLeft) { move_left }
-      when_key(Gosu::Button::KbRight) { move_right }
-      when_key(Gosu::Button::KbUp) { move_up }
-      when_key(Gosu::Button::KbDown) { move_down }
-
-      when_key(Gosu::Button::KbSpace) { balls.each { |ball| ball.speed, ball.old_speed = ball.old_speed, ball.speed }  }
+    
+  balls.each do |ball|
+    representation Nimo::Quad, :for => ball, :with => {:color => ball.color} do
+      always { move }
+      listen_to(:color_change) { |representation, object| representation.color = ball.color }
     end
   end
-  
+
+  representation(Nimo::Quad, :for => pad, :with => {:color => Gosu::white}) do
+    when_key(Gosu::Button::KbLeft) { move_left }
+    when_key(Gosu::Button::KbRight) { move_right }
+    when_key(Gosu::Button::KbUp) { move_up }
+    when_key(Gosu::Button::KbDown) { move_down }
+
+    when_key(Gosu::Button::KbSpace) { balls.each { |ball| ball.speed, ball.old_speed = ball.old_speed, ball.speed }  }
+  end
+    
   when_key(Gosu::Button::KbEscape) { exit }
   
 end
---
+++ examples/simplest.rb
@@ -0,0 +1,22 @@
+# simplest.rb
+# 
+# Simplest example ever! A white box controlled with the arrow keys.
+# 
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'nimo'
+
+Nimo::GameWindow.new("Simplest", 640, 480) do
+  
+  screen :game do
+    quad :with => { :width => 20, :height => 20, :color => Gosu::white } do
+      when_key(Gosu::Button::KbLeft)  { x -= 5 }
+      when_key(Gosu::Button::KbRight) { x += 5 }
+      when_key(Gosu::Button::KbUp)    { y -= 5 }
+      when_key(Gosu::Button::KbDown)  { y += 5 }
+    end
+    
+    when_key(Gosu::Button::KbEscape) { exit }
+  end
+  
+end.show

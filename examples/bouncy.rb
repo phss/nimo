1e2f607f79c6dd77781537cad61262da456530d5
moonpxi <paulo.schneider@gmail.com>
2009-09-12 21:36:20 +0100

    Fixing bouncy example

--- examples/bouncy.rb
+++ examples/bouncy.rb
@@ -1,4 +1,3 @@
-# 
 # bouncy.rb
 # 
 # Example using deflectors, projectiles and moveable behaviors. It demonstrates how to create interacting
@@ -9,60 +8,10 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'nimo'
+include Gosu::Button
 
 WINDOW_WIDTH = 512
 WINDOW_HEIGHT = 480
-
-# class GameScreen < Nimo::Screen
-#   
-#   def load
-#     pad = Pad.new
-#     balls = (0..19).collect { Ball.new(pad, Wall.sections) }
-#     
-#     balls.each do |ball|
-#       ball.with_deflectors(balls.find_all { |other_ball| other_ball != ball })
-#       
-#       add(Nimo::QuadRepresentation, :for => ball, :with => {:color => ball.color}).
-#         always { move }.
-#         listen_to(:color_change) { |representation, object| representation.color = ball.color }
-#     end
-#       
-#     add(Nimo::QuadRepresentation, :for => pad, :with => {:color => Gosu::white}).
-#       when_key(Gosu::Button::KbLeft) { move_left }.
-#       when_key(Gosu::Button::KbRight) { move_right }.
-#       when_key(Gosu::Button::KbUp) { move_up }.
-#       when_key(Gosu::Button::KbDown) { move_down }.
-#       when_key(Gosu::Button::KbSpace) { balls.each { |ball| ball.speed,ball.old_speed = ball.old_speed,ball.speed }  }
-#   end
-#   
-#   def button_down(id)
-#     exit if id == Gosu::Button::KbEscape
-#   end
-#   
-# end
-
-class GameScreen < Nimo::Screen
-  
-  object[:balls].each do |ball|
-    quad :for => ball, :with => {:color => ball.color} do
-      always { move }
-      listen_to(:color_change) { |representation, object| representation.color = object.color }
-    end
-  end
-
-  quad :for => object[:pad], :with => {:color => Gosu::white} do
-    when_key(Gosu::Button::KbLeft) { move_left }
-    when_key(Gosu::Button::KbRight) { move_right }
-    when_key(Gosu::Button::KbUp) { move_up }
-    when_key(Gosu::Button::KbDown) { move_down }
-
-    when_key(Gosu::Button::KbSpace) { balls.each { |ball| ball.speed, ball.old_speed = ball.old_speed, ball.speed }  }
-  end
-    
-  when_key(Gosu::Button::KbEscape) { exit }
-  
-end
-
 
 class Pad < Nimo::GameObject
   include Nimo::Behavior::Deflector
@@ -99,10 +48,9 @@ class Ball < Nimo::GameObject
  
   attr_accessor :speed, :old_speed
 
-
   def initialize(*deflectors)
     random_velocity = Nimo::Behavior::Velocity.new(0, 1)
-    random_velocity.adjust(rand + -rand)
+    random_velocity.adjust(rand)
     
     super(:x => 200, :y => 200, :width => 10, :height => 10, :velocity => random_velocity, :speed => 10, :old_speed => 0)
     with_deflectors(deflectors)
@@ -125,9 +73,29 @@ end
 if __FILE__ == $PROGRAM_NAME
   pad = Pad.new
   balls = (0..19).collect { Ball.new(pad, Wall.sections) }
+  
+  Nimo::Game("Bouncy", WINDOW_WIDTH, WINDOW_HEIGHT) do
+    
+    screen :game do
+      balls.each do |ball|
+        quad :for => ball, :with => {:color => ball.color} do
+          always { move }
+          listen_to(:color_change) { |representation, object| representation.color = object.color }
+        end
+      end
 
-  window = Nimo::GameWindow.new("Bouncy", WINDOW_WIDTH, WINDOW_HEIGHT) do
-    screen GameScreen, :pad => pad, :balls => balls
+      quad :for => pad, :with => {:color => Gosu::white} do
+        when_key(KbLeft) { move_left }
+        when_key(KbRight) { move_right }
+        when_key(KbUp) { move_up }
+        when_key(KbDown) { move_down }
+
+        when_key(KbSpace) { balls.each { |ball| ball.speed, ball.old_speed = ball.old_speed, ball.speed }  }
+      end
+
+      when_key(KbEscape) { exit }
+    end
+    
   end
-  window.show
+  
 end
--- examples/simplest.rb
+++ examples/simplest.rb
@@ -5,18 +5,19 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'nimo'
+include Gosu::Button
 
 Nimo::Game("Simplest", 640, 480) do
   
   screen :game do
     quad :with => { :width => 20, :height => 20, :color => Gosu::white } do
-      when_key(Gosu::Button::KbLeft)  { self.x -= 5 }
-      when_key(Gosu::Button::KbRight) { self.x += 5 }
-      when_key(Gosu::Button::KbUp)    { self.y -= 5 }
-      when_key(Gosu::Button::KbDown)  { self.y += 5 }
+      when_key(KbLeft)  { self.x -= 5 }
+      when_key(KbRight) { self.x += 5 }
+      when_key(KbUp)    { self.y -= 5 }
+      when_key(KbDown)  { self.y += 5 }
     end
     
-    when_key(Gosu::Button::KbEscape) { exit }
+    when_key(KbEscape) { exit }
   end
   
 end

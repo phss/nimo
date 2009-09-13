88c318e2971bca1d4e5f9354672ae4c5a1b5e0e3
moonpxi <paulo.schneider@gmail.com>
2009-09-13 20:40:47 +0100

    Fixing spritey example

--- examples/spritey.rb
+++ examples/spritey.rb
@@ -6,55 +6,11 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'nimo'
+include Gosu::Button
 
 WINDOW_WIDTH = 512
 WINDOW_HEIGHT = 480
 WINDOW = {:x => 0, :y => 0, :width => WINDOW_WIDTH, :height => WINDOW_HEIGHT}
-
-class TitleScreen < Nimo::Screen
-  
-  def load
-    add(Nimo::QuadRepresentation, :with => WINDOW.merge(:color => Gosu::white))
-    add(Nimo::ImageRepresentation, :with => {:x => 116, :y => 190, :image => :jeeklabs})
-  end
-  
-  def button_down(id)
-    go_to(:Game)
-  end
-  
-end
-
-class GameScreen < Nimo::Screen
-  
-  def load
-		player_observer = Proc.new do |rep, obj|
-		  rep.flip if obj.velocity.x < 0
-		  rep.unflip if obj.velocity.x > 0	
-
-			rep.change_to(:stopped) if obj.velocity.x == 0
-			rep.change_to(:walking) if obj.velocity.x != 0
-			rep.change_to(:jumping) if obj.velocity.y < 0
-			rep.change_to(:falling) if obj.velocity.y > 0
-		end
-
-    add(Nimo::QuadRepresentation, :with => WINDOW.merge(:color => Gosu::white))
-    add(Nimo::SpriteRepresentation, :for => Player.new, :with => {:image => :tcheco}).
-      always { move }.
-      when_key(Gosu::Button::KbLeft) { move_left }.
-      when_key(Gosu::Button::KbRight) { move_right }.
-      when_key(Gosu::Button::KbUp) { jump }.
-      with_animation(:stopped, [0]).
-      with_animation(:walking, [1, 2, 3, 4]).
-      with_animation(:jumping, [5, 6, 7], :loop => false).
-      with_animation(:falling, [8, 9], :loop => false).
-			with_observer(&player_observer)
-  end
-  
-  def button_down(id)
-    exit if id == Gosu::Button::KbEscape
-  end
-  
-end
 
 class Player < Nimo::GameObject
 	include Nimo::Behavior::WithVelocity
@@ -93,11 +49,42 @@ end
 
 
 if __FILE__ == $PROGRAM_NAME
-  window = Nimo::GameWindow.new("Spritey", WINDOW_WIDTH, WINDOW_HEIGHT)
-  window.global_resources.
-    with_image(:jeeklabs, "examples/images/jeeklabs.png").
-    with_image_tiles(:tcheco, "examples/images/tcheco.png", 48, 62)
-  
-  window.add_screens_by_class(TitleScreen, GameScreen)
-  window.show
+  Nimo::Game("Spritey", WINDOW_WIDTH, WINDOW_HEIGHT) do
+    images :jeeklabs => { :filename => "examples/images/jeeklabs.png" },
+           :tcheco => { :filename => "examples/images/tcheco.png", :tile_dimension => [48, 62] }
+           
+    screen :title do
+      quad :with => WINDOW.merge(:color => Gosu::white)
+      image :with => {:x => 116, :y => 190, :image => :jeeklabs}
+      
+      any_key { go_to :game }
+    end
+    
+    screen :game do
+      player_observer = Proc.new do |rep, obj|
+  		  rep.flip if obj.velocity.x < 0
+  		  rep.unflip if obj.velocity.x > 0	
+
+  			rep.change_to(:stopped) if obj.velocity.x == 0
+  			rep.change_to(:walking) if obj.velocity.x != 0
+  			rep.change_to(:jumping) if obj.velocity.y < 0
+  			rep.change_to(:falling) if obj.velocity.y > 0
+  		end
+
+      quad :with => WINDOW.merge(:color => Gosu::white)
+      sprite :for => Player.new, :with => {:image => :tcheco} do
+        always { move }
+        when_key(KbLeft) { move_left }
+        when_key(KbRight) { move_right }
+        when_key(KbUp) { jump }
+        with_animation(:stopped, [0])
+        with_animation(:walking, [1, 2, 3, 4])
+        with_animation(:jumping, [5, 6, 7], :loop => false)
+        with_animation(:falling, [8, 9], :loop => false)
+  			with_observer(&player_observer)
+			end
+  			
+  		when_key(KbEscape) { exit }
+    end
+  end
 end
--- lib/nimo/representations/sprite_representation.rb
+++ lib/nimo/representations/sprite_representation.rb
@@ -1,76 +1,77 @@
 module Nimo
   
   class SpriteRepresentation < ObjectRepresentation
-      attr_accessor :flip
-      
-      def initialize(game_window, game_object)
-        super(game_window, game_object)
-        @animations = {}
-        @current_animation = nil
-				@next_animation = nil
-        unflip
-      end
+    represent :sprite  
+    attr_accessor :flip
+    
+    def initialize(game_window, game_object)
+      super(game_window, game_object)
+      @animations = {}
+      @current_animation = nil
+			@next_animation = nil
+      unflip
+    end
 
-      def load(resources, params)
-        @sprite_tiles = resources.images[params[:image]]        
-      end
-      
-      def flip
-        @drawing = lambda { |sprite| sprite.draw(@game_object.x + @game_object.width, @game_object.y, 0, -1) }
-      end
-      
-      def unflip
-        @drawing = lambda { |sprite| sprite.draw(@game_object.x, @game_object.y, 0) }
-      end
-
-			def change_to(animation_name)
-				@next_animation = animation_name
-			end
-
-      def draw
-        if @current_animation.name != @next_animation && @animations.has_key?(@next_animation)
-           @current_animation = @animations[@next_animation]
-           @current_animation.reset_animation
-        end
-        @drawing.call(@sprite_tiles[@current_animation.frame_index])
-      end
-      
-      # Adds a new animation to the sprite. The first animation will be set as the current.
-      # A list of options can be specified to customise the behavior:
-      # - :timeout (default is 0.1): time to wait between frames
-      # - :loop (default is true): true if the animation should start from the beginning, after the last element,
-      #       otherwise it will draw the last frame
-      def with_animation(name, frame_indexes, options = {})
-        @animations[name] = Animation.new(name, frame_indexes, options)
-        @current_animation = @animations[name] if @current_animation.nil?
-        self
-      end
+    def load(resources, params)
+      @sprite_tiles = resources.images[params[:image]]        
     end
     
-    class Animation
-      attr_reader :name
-      
-      def initialize(name, frame_indexes, options)
-        @name = name
-        @frame_indexes = frame_indexes
-        @options = {:timeout => 0.1, :loop => true}.merge(options)
-        reset_animation
-      end
-      
-      def reset_animation
-        @current_index = 0
-        @time_since_last_draw = Time.now
-      end
-      
-      def frame_index
-        if (Time.now - @time_since_last_draw) > @options[:timeout]
-          @time_since_last_draw = Time.now
-          @current_index += 1
-          if @current_index == @frame_indexes.size
-            @current_index = @options[:loop] ? 0 : @frame_indexes.size - 1
-          end
-        end
-        @frame_indexes[@current_index]
-      end
+    def flip
+      @drawing = lambda { |sprite| sprite.draw(@game_object.x + @game_object.width, @game_object.y, 0, -1) }
     end
+    
+    def unflip
+      @drawing = lambda { |sprite| sprite.draw(@game_object.x, @game_object.y, 0) }
+    end
+
+		def change_to(animation_name)
+			@next_animation = animation_name
+		end
+
+    def draw
+      if @current_animation.name != @next_animation && @animations.has_key?(@next_animation)
+         @current_animation = @animations[@next_animation]
+         @current_animation.reset_animation
+      end
+      @drawing.call(@sprite_tiles[@current_animation.frame_index])
+    end
+    
+    # Adds a new animation to the sprite. The first animation will be set as the current.
+    # A list of options can be specified to customise the behavior:
+    # - :timeout (default is 0.1): time to wait between frames
+    # - :loop (default is true): true if the animation should start from the beginning, after the last element,
+    #       otherwise it will draw the last frame
+    def with_animation(name, frame_indexes, options = {})
+      @animations[name] = Animation.new(name, frame_indexes, options)
+      @current_animation = @animations[name] if @current_animation.nil?
+      self
+    end
+  end
+  
+  class Animation
+    attr_reader :name
+    
+    def initialize(name, frame_indexes, options)
+      @name = name
+      @frame_indexes = frame_indexes
+      @options = {:timeout => 0.1, :loop => true}.merge(options)
+      reset_animation
+    end
+    
+    def reset_animation
+      @current_index = 0
+      @time_since_last_draw = Time.now
+    end
+    
+    def frame_index
+      if (Time.now - @time_since_last_draw) > @options[:timeout]
+        @time_since_last_draw = Time.now
+        @current_index += 1
+        if @current_index == @frame_indexes.size
+          @current_index = @options[:loop] ? 0 : @frame_indexes.size - 1
+        end
+      end
+      @frame_indexes[@current_index]
+    end
+  end
 end
